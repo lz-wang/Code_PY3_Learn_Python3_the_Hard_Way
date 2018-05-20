d3559c0906e0fcd0d0d17e03f4ef0fafa6d77d61
zzSWG <zhuangwang82@gmail.com>
2018-05-20 16:37:10 +0800

    add code for ex 01~05
    Exercise 1. A Good First Program
    
    Exercise 2. Comments and Pound Characters
    	*
    Exercise 3. Numbers and Math
    
    Exercise 4. Variables and Names
    
    Exercise 5. More Variables and Printing

---
+++ ex05.py
@@ -0,0 +1,18 @@
+my_name = 'LZ Wang'
+my_age = 24 # not a lie
+my_height = 175 # cm
+my_weight = 72 # kg
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} centimeters tall.")
+print(f"He's {my_weight} kilograms heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height} and {my_weight} I get {my_age + my_height + my_weight}.")
